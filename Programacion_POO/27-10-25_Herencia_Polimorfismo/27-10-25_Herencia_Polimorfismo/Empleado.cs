7abd1dfd4fa34a81431f2d1f9a3cfa696042ea4f
Daniela Serra <daniserra.it@gmail.com>
2025-10-29 11:45:17 -0300

    Añade clase de Herencia y Polimorfismo: métodos sobreescritos y virtuales

---
+++ Programacion_POO/27-10-25_Herencia_Polimorfismo/27-10-25_Herencia_Polimorfismo/Empleado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Empleado
+{
+    public class Empleado
+    {
+        string nombre;
+        int legajo;
+        double salarioBase;
+
+        public Empleado(string nombre, int legajo, double salario)
+        {
+            this.nombre = nombre;
+            this.legajo = legajo;
+            this.salarioBase = salario;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Legajo { get => legajo; set => legajo = value; }
+        public double SalarioBase { get => salarioBase; set => salarioBase = value; }
+
+        public virtual string MostrarInfo()
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"nombre: {nombre}");
+            sb.AppendLine($"legajo: {legajo}");
+            sb.AppendLine($"salario base: {salarioBase}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Programacion_POO/27-10-25_Herencia_Polimorfismo/Testeo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Empleado;
+
+namespace Testeo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado e1 = new Empleado("Juan", 1, 1000);
+
+        }
+    }
+}
