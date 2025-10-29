842dd58df04ebe48b94f1671398cec230e92d210
Daniela Serra <daniserra.it@gmail.com>
2025-10-29 11:42:25 -0300

    Añade clase de Objetos: definición y atributos

---
+++ Programacion_POO/04-09-25-objetos/04-09-25-objetos/Alumno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_09_25_objetos
+{
+    public class Alumno
+    {
+        public String nombre;
+        public String apellido;
+        public int dni;
+        public float nota;
+
+        public void Estudiar()
+        {
+            Console.WriteLine("Estoy estudiando");
+        }
+
+        public int RendirExamen()
+        {
+            Random r = new Random();
+            int nota = r .Next(1, 10);
+
+            return nota;
+        }
+       
+
+       
+    }
+}
---
+++ Programacion_POO/04-09-25-objetos/04-09-25-objetos/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _04_09_25_objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar objeto
+            // Crear objeto en memoria
+            // Usando operador new (crea espacio fisico en memoria para crear objeto de clase Alumno)
+            Alumno miAlumno = new Alumno();
+            // Alumno() es el constructor de la clase
+
+            //Console.WriteLine(miAlumno); // Si pongo un objeto en el ConsoleWriteLine me dice qué tipo de objeto es (clase Alumno)
+
+            miAlumno.nombre = "Juan";
+            miAlumno.apellido = "Ruiz";
+            miAlumno.dni = 2222222;
+
+            miAlumno.Estudiar();
+
+            miAlumno.nota = miAlumno.RendirExamen();
+
+            Console.WriteLine($"La nota de {miAlumno.nombre} es {miAlumno.nota}");
+            
+
+        }
+    }
+}
